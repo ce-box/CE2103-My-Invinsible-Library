35b8ce69c41cb876df125d346648c67533f2151c
Esteban Alvarado <estalvgs1999@gmail.com>
2019-06-02 21:23:11 -0600

    Servidor 2.0 IMPLEMENTADO
    Este server permite enviar y recibir JSON en todos sus métodos.
    ** USAR ESTE PARA EL DESARROLLO DEL PROYECTO**

---
+++ Servidor 2.0/src/main/com/tec/MILIB/Resources/MilibRestService.java
@@ -0,0 +1,63 @@
+package main.com.tec.MILIB.Resources;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class MilibRestService {
+
+    /**
+     * POST
+     * http://ip_addr:8080/MILIB_Servidor_war_exploded/api/insert
+     */
+
+    @POST
+    @Path("/insert")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response insert(InputStream incomingData){
+
+        // Setting an String Builder
+        StringBuilder inputBuilder = new StringBuilder();
+
+        // Try to read the incomingData in JSON format
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+            String line = null;
+            while((line = in.readLine())!= null){
+                inputBuilder.append(line);
+            }
+        } catch (Exception e){
+            System.out.println("Error Parsing: -");
+        }
+
+        // Show in console the received data
+        System.out.println("Data Received: "+ inputBuilder.toString());
+
+        // Return HTTP response 200 in case of success
+        return Response.status(200).entity(inputBuilder.toString()).build();
+    }
+
+    /**
+     * GET
+     * http://ip_addr:8080/MILIB_Servidor_war_exploded/api/select
+     */
+
+    @GET
+    @Path("/select")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response select(InputStream incomingData){
+        String result = "MILIB Rest Service Successfully started...";
+
+        return Response.status(200).entity(result).build();
+    }
+}
---
+++ Servidor 2.0/web/index.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: esteban
+  Date: 02/06/19
+  Time: 03:31 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>MILIB - My Invensible Library</title>
+  </head>
+  <body>
+  </body>
+</html>
