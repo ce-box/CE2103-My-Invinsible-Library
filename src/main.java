f8670003783b54f1b9d066463eb649ca1963717c
Juan <juanpo.alvarado@gmail.com>
2019-06-03 18:22:35 -0600

    Json esperado

--- src/main.java
+++ src/main.java
@@ -7,14 +7,14 @@ public class main {
 
         ArrayList<String> L = new ArrayList<String>();
         L.add("name");
-        L.add("date");
+        L.add("size");
 //        ArrayList<String> L2= new ArrayList<String>();
 //        L2.add("date");
 //        ArrayList<String> L3= new ArrayList<String>();
 //        L3.add("2019");
         ArrayList<String> L4= new ArrayList<String>();
-        L4.add("Sahid");
-        L4.add("2019");
+        L4.add("Wajo");
+        L4.add("200.0");
         Metadata.Insert(L,L4);
         Metadata.Select(null);
 
