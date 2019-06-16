f755d280183db3cc1cc567553c04b226fb01fff6
Esteban Alvarado <estalvgs1999@gmail.com>
2019-06-15 23:56:22 -0600

    Más cambios

--- Servidor MetaData/src/main/com/tec/MILIB_DB/domain/Node.java
+++ Servidor MetaData/src/main/com/tec/MILIB_DB/domain/Node.java
@@ -1,4 +1,21 @@
 package main.com.tec.MILIB_DB.domain;
 
-public class Node {
-}
+class Node
+{
+    char ch;
+    int freq;
+    Node left = null, right = null;
+
+    Node(char ch, int freq)
+    {
+        this.ch = ch;
+        this.freq = freq;
+    }
+
+    public Node(char ch, int freq, Node left, Node right) {
+        this.ch = ch;
+        this.freq = freq;
+        this.left = left;
+        this.right = right;
+    }
+};
